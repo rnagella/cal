a874d6740e2eabdcce9d9f7789d5289e636f4eff
Ranjith Nagella <nagella.ranjith@gmail.com>
2016-02-07 02:22:27 -0500

    changes to UI, logic updates.

--- index.ios.js
+++ index.ios.js
@@ -5,22 +5,27 @@ var {
   StyleSheet,
   View,
   Text,
-  TouchableHighlight
+  TouchableHighlight,
+  StatusBarIOS
 } = React;
+
+StatusBarIOS.setStyle(1);
 
 // create a component
 var Cal = React.createClass({
   getInitialState: function () {
     return {
       isAC: true,
-      memory: 0, // memory varialble
-      current: 0, // add value of display
-      operation: 0, // records code for +, -, * and / etc.,
-      MAXLENGHT: 15,
-      buttonBorderColor: 'green',
+      memory: 0, // memory varialble - previous number you entered
+      current: 0, // the number currently being entered
+      operation: 0, // records code for +, -, * and / etc., - the function button we pushed
+      MAXLENGHT: 30, // constant 
+      buttonBorderColor: 'grey',
       buttonBorderRightWidth: 1,
       buttonBorderLeftWidth: 1,
-      buttonUnderlayColor: 'green'
+      buttonUnderlayColor: 'grey',
+      isOperationSelected: false,
+      lastCurrentvalue: 0
     }
   },
   render: function () {
@@ -72,14 +77,15 @@ var Cal = React.createClass({
       borderBottomWidth: bottomWidth
     }
   },
-  specialButtonBorder: function () {
+  specialButtonBorder: function (color) {
     return {
       borderBottomWidth: 1,
       borderBottomColor: 'gray'
     }
   },
   acButton: function () {
-    return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}  
+    return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}
+            onPress = {this.addDigit.bind(this, 'AC/C')}  
             style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         {this.state.isAC ? 'AC': 'C'}
@@ -87,7 +93,8 @@ var Cal = React.createClass({
     </TouchableHighlight>
   },
   plusMinusButton: function () {
-    return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor} 
+    return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}
+      onPress = {this.addDigit.bind(this, '+/-')} 
       style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         +/-
@@ -96,6 +103,7 @@ var Cal = React.createClass({
   },
   percentageButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor} 
+    onPress = {this.addDigit.bind(this, '%')}
     style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         %
@@ -104,6 +112,7 @@ var Cal = React.createClass({
   },
   divButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor} 
+      onPress = {this.addDigit.bind(this, '÷')}
       style={[styles.specialButtonView, this.specialButtonBorder()]}>
       <Text style={styles.text}>
         ÷
@@ -112,7 +121,7 @@ var Cal = React.createClass({
   },
   sevenButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}
-      onPress = {this.addDigit.bind(this, 7)}   
+      onPress = {this.digit.bind(this, 7)}   
       style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         7
@@ -121,7 +130,7 @@ var Cal = React.createClass({
   },
   eightButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}
-      onPress = {this.addDigit.bind(this, 8)}   
+      onPress = {this.digit.bind(this, 8)}   
       style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         8
@@ -130,7 +139,7 @@ var Cal = React.createClass({
   },
   nineButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}
-      onPress = {this.addDigit.bind(this, 9)}   
+      onPress = {this.digit.bind(this, 9)}   
       style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         9
@@ -139,6 +148,7 @@ var Cal = React.createClass({
   },
   mulButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}  
+      onPress = {this.addDigit.bind(this, '*')}
       style={[styles.specialButtonView, this.specialButtonBorder()]}>
       <Text style={styles.text}>
         *
@@ -147,7 +157,7 @@ var Cal = React.createClass({
   },
   fourButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor} 
-      onPress = {this.addDigit.bind(this, 4)}  
+      onPress = {this.digit.bind(this, 4)}  
       style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         4
@@ -156,7 +166,7 @@ var Cal = React.createClass({
   },
   fiveButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}
-      onPress = {this.addDigit.bind(this, 5)}   
+      onPress = {this.digit.bind(this, 5)}   
       style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         5
@@ -165,7 +175,7 @@ var Cal = React.createClass({
   },
   sixButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}  
-      onPress = {this.addDigit.bind(this, 6)} 
+      onPress = {this.digit.bind(this, 6)} 
       style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         6
@@ -174,6 +184,7 @@ var Cal = React.createClass({
   },
   minuButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}  
+    onPress = {this.addDigit.bind(this, '-')}
     style={[styles.specialButtonView, this.specialButtonBorder()]}>
       <Text style={styles.text}>
         -
@@ -182,7 +193,7 @@ var Cal = React.createClass({
   },
   oneButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}
-      onPress = {this.addDigit.bind(this, 1)}
+      onPress = {this.digit.bind(this, 1)}
       style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         1
@@ -191,7 +202,7 @@ var Cal = React.createClass({
   },
   twoButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}
-    onPress = {this.addDigit.bind(this, 2)}  
+    onPress = {this.digit.bind(this, 2)}  
     style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         2
@@ -200,7 +211,7 @@ var Cal = React.createClass({
   },
   threeButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor} 
-      onPress = {this.addDigit.bind(this, 3)} 
+      onPress = {this.digit.bind(this, 3)} 
       style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, this.state.buttonBorderLeftWidth)]}>
       <Text style={styles.text}>
         3
@@ -208,9 +219,9 @@ var Cal = React.createClass({
     </TouchableHighlight>
   },
   plusButton: function () {
-    return <TouchableHighlight 
+    return <TouchableHighlight
       ref="plusButton" underlayColor={this.state.buttonUnderlayColor} 
-      onPress = {this.addDigit.bind(this, '+')}   
+      onPress = {this.addDigit.bind(this, '+')}
       style={[styles.specialButtonView, this.specialButtonBorder()]}>
       <Text style={styles.text}>
         +
@@ -219,7 +230,7 @@ var Cal = React.createClass({
   },
   zeroButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor} 
-      onPress = {this.addDigit.bind(this, 0)} 
+      onPress = {this.digit.bind(this, 0)} 
       style={[styles.zeroButton]}>
       <Text style={styles.text}>
         0
@@ -228,7 +239,7 @@ var Cal = React.createClass({
   },
   periodButton: function () {
     return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}
-      onPress = {this.addDigit.bind(this, '.')}  
+      onPress = {this.digit.bind(this, '.')}  
       style={[styles.buttonView, this.border(this.state.buttonBorderColor, this.state.buttonBorderRightWidth, 0)]}>
       <Text style={styles.text}>
         .
@@ -236,44 +247,202 @@ var Cal = React.createClass({
     </TouchableHighlight>
   },
   equalButton: function () {
-    return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}  style={[styles.specialButtonView]}>
+    return <TouchableHighlight underlayColor={this.state.buttonUnderlayColor}  
+    onPress = {this.addDigit.bind(this, '=')}
+    style={[styles.specialButtonView]}>
       <Text style={styles.text}>
         =
       </Text>
     </TouchableHighlight>
   },
-  _onPressButton: function (event, value) {
-    console.log(event);
-  },
-  addDigit: function (dig, event) {
-    switch (dig) {
-      case '+':
-        console.log('plusButton');
+  operate: function (dig) {
+    if (this.state.operation == 0) {
+      this.setState({
+        memory: this.state.current,
+        current: '', // or we could use 0
+        operation: dig,
+        isOperationSelected: true
+      });       
+    } else {
+      if (this.state.isOperationSelected == true) {
         return;
+      }
+      if (this.state.operation == dig) {
+        if (this.state.operation == 1) {
+          memoryVal = eval(this.state.memory) * eval(this.state.current); 
+        } else if (this.state.operation == 2) {
+          memoryVal = eval(this.state.memory) / eval(this.state.current); 
+        } else if (this.state.operation == 3) {
+          memoryVal = eval(this.state.memory) + eval(this.state.current); 
+        } else if (this.state.operation == 4) {
+          memoryVal = eval(this.state.memory) - eval(this.state.current); 
+        }
+        this.setState({
+          memory: memoryVal,
+          current: memoryVal,
+          isOperationSelected: true,
+        });
+      } else {
+        if (this.state.operation == 1) {
+          memoryVal = eval(this.state.memory) * eval(this.state.current); 
+        } else if (this.state.operation == 2) {
+          memoryVal = eval(this.state.memory) / eval(this.state.current); 
+        } else if (this.state.operation == 3) {
+          memoryVal = eval(this.state.memory) + eval(this.state.current); 
+        } else if (this.state.operation == 4) {
+          memoryVal = eval(this.state.memory) - eval(this.state.current); 
+        }
+        this.setState({
+          memory: memoryVal,
+          current: memoryVal,
+          isOperationSelected: true
+        });
+      }
     }
-    
+  },
+  digit: function (dig, event) {
     if (this.state.current.toString().length > this.state.MAXLENGHT) {
       this.setState({
         current: 'Aargh! Too long'
       });
     } else {
+      // set the value to 0 if the current is a string of 'Aargh! Too long' 
+      if (this.state.current == 'Aargh! Too long') {
+        this.setState({
+          lastCurrentvalue: 0,
+          current: 0
+        });
+      }
+
+      if (this.state.current == 'Infinity') {
+        this.setState({
+          lastCurrentvalue: 0,
+          current: 0,
+          memory: 0
+        });
+      }
+
       if (this.state.current == 0 && this.state.current.toString().indexOf('.') == -1) {
         this.setState({
-          current: dig
+          lastCurrentvalue: 0 + dig,
+          current: 0 + dig
         });
       } else {
         if (dig == '.' && this.state.current.toString().indexOf('.')  != -1) {
           return;
         } else {
-          this.setState({
-            current: this.state.current + dig.toString()
-          });
+          if (!this.state.isOperationSelected) {
+            this.setState({
+              lastCurrentvalue: this.state.current + dig.toString(),
+              current: this.state.current + dig.toString()
+            });
+          } else {
+            this.setState({
+              lastCurrentvalue: dig.toString(),
+              current: dig.toString()
+            });
+          }
         }
       }
+      this.setState({
+      isOperationSelected: false,
+      isAC: false
+    });
     }
   },
-  onShowUnderlay: function () {
-    console.log('onShowUnderlay');
+  addDigit: function (dig, event) {
+    switch (dig) {
+      case '=': 
+        var memoryVal;
+        if (this.state.operation == 1) {
+          memoryVal = eval(this.state.memory) * eval(this.state.lastCurrentvalue); 
+        } else if (this.state.operation == 2) {
+          memoryVal = eval(this.state.memory) / eval(this.state.lastCurrentvalue); 
+        } else if (this.state.operation == 3) {
+          memoryVal = eval(this.state.memory) + eval(this.state.lastCurrentvalue); 
+        } else if (this.state.operation == 4) {
+          memoryVal = eval(this.state.memory) - eval(this.state.lastCurrentvalue); 
+        }
+        this.setState({
+          memory: memoryVal,
+          current: memoryVal,
+          isOperationSelected: true
+        });
+        return;
+      case '*': 
+        this.operate(1);
+        if (this.state.operation !== 0) {
+          this.setState({
+            operation: 1,
+            isOperationSelected: true
+          });
+        }
+        return;
+      case '÷':
+        this.operate(2);
+        if (this.state.operation !== 0) {
+          this.setState({
+            operation: 2,
+            isOperationSelected: true
+          });
+        }
+        return;
+      case '+':
+        this.operate(3);
+        if (this.state.operation !== 0) {
+          this.setState({
+            operation: 3,
+            isOperationSelected: true
+          });
+        }
+        return;
+      case '-': 
+      this.operate(4);
+      if (this.state.operation !== 0) {
+          this.setState({
+            operation: 4,
+            isOperationSelected: true
+          });
+        }
+        return;
+      case '%':
+        console.log('percentageButton');
+        this.setState({
+          memory: (this.state.current)/100,
+          current: (this.state.current)/100, 
+        });
+        return;
+      case '+/-':
+        if (this.state.current !== 0) {
+          if (this.state.current.toString().indexOf('-') == 0) {
+            this.setState({
+              current: this.state.current.substring(1)
+            });
+          } else {
+            this.setState({
+              current: '-' + this.state.current
+            });
+          }
+        }
+      return;
+      case 'AC/C':
+        console.log('acButton');
+        if (!this.state.isAC) {
+          // clear current
+          this.setState({
+            current: 0
+          });
+        } else {
+          // clear everything
+          this.setState({
+            current: 0,
+            operation: 0,
+            memory: 0,
+            isAC: true
+          });
+        }
+        return;
+    }
   }
 });
 
@@ -283,8 +452,9 @@ var styles = StyleSheet.create({
     flex: 1,
   },
   text: {
-    fontSize: 18, 
-    fontWeight: 'bold'
+    fontSize: 32, 
+    fontWeight: '100',
+    color: '#fff'
   },
   resultView: {
     flex: 2,
@@ -294,7 +464,7 @@ var styles = StyleSheet.create({
   },
   resultText: {
     fontSize: 48,
-    fontWeight: 'normal',
+    fontWeight: '300',
     color: '#EBEBEB',
     right: 10
   },
@@ -309,23 +479,23 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor: 'red',  
+    backgroundColor: '#2c3e50',  
   },
   zeroButton: {
     flex: 2,
     justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor: 'red',
+    backgroundColor: '#2c3e50',
     borderRightWidth: 1,
+    borderRightColor: 'grey',
     borderLeftWidth: 1,
-    borderLeftColor: 'white',
-    borderRightColor: 'green',
+    borderLeftColor: '#2c3e50'
   },
   specialButtonView: {
-    backgroundColor: '#EEBC3F',
+    backgroundColor: '#3B7A57',
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1
+    flex: 1,
   }
 });
 
